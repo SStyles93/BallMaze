8f65e65144b72d9380301199df42ddfbcd7e86fa
SStyles93 <samuel.styles93@gmail.com>
2026-01-12 12:23:38 -0800

    Update Star particles -> Emissive set at start

---
+++ Assets/02_Scripts/98_VisualEffects/StarParticleUpdate.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class StarParticleUpdate : MonoBehaviour
+{
+    private ParticleSystem starParticleSystem;
+
+    private ParticleSystem.MinMaxGradient starColor;
+
+    void Awake()
+    {
+        starParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        starColor = starParticleSystem.main.startColor;
+        GetComponent<ParticleSystemRenderer>().material.SetColor("_EmissionColor", starColor.color * 3.0f);
+        
+    }
+}
