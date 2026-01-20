26b542fa4d31f5e79892e0e8d730c64ef739aebb
SStyles93 <samuel.styles93@gmail.com>
2026-01-19 16:06:53 -0800

    update Pannels

--- Assets/02_Scripts/5_UI/Pannels/InsufficientFundsPannelManager.cs
+++ Assets/02_Scripts/5_UI/Pannels/InsufficientFundsPannelManager.cs
@@ -16,12 +16,12 @@ public class InsufficientFundsPannelManager : MonoBehaviour
             case CoinType.COIN:
                 shopButton.SetActive(true);
                 homeButton.SetActive(false);
-                pannelText.text = $"Insufficient <sprite index={(int)coinType}>";
+                pannelText.text = $"Not enough <sprite index={(int)coinType}>";
                 break;
             case CoinType.STAR:
                 homeButton.SetActive(true);
                 shopButton.SetActive(false);
-                pannelText.text = $"Insufficient <sprite index={(int)coinType}>";
+                pannelText.text = $"Not enough <sprite index={(int)coinType}>";
                 break;
         }
     }
