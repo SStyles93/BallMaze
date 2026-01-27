1bbf091fbac83cf4e5ff736feda2a23426178b1a
SStyles93 <samuel.styles93@gmail.com>
2026-01-27 14:16:36 -0800

    Update Textures & Colours

--- Assets/02_Scripts/2_PCG/Generator/Maze/StarPlacer.cs
+++ Assets/02_Scripts/2_PCG/Generator/Maze/StarPlacer.cs
@@ -6,7 +6,8 @@ namespace PxP.PCG
 {
     public static class StarPlacer
     {
-        public static void PlaceStars(CellData[,] grid, GeneratorParameters_SO p,HashSet<Vector2Int> walkable,
+
+        public static void PlaceStars(CellData[,] grid, GeneratorParameters_SO p, HashSet<Vector2Int> walkable,
             Vector2Int start, Vector2Int end, System.Random rng)
         {
             var placed = new List<Vector2Int>();
@@ -16,18 +17,33 @@ namespace PxP.PCG
 
             GridUtils.Shuffle(candidates, rng);
 
+            // First pass: try respecting minStarDistance
             foreach (var pos in candidates)
             {
                 if (placed.Count >= p.starCount) break;
                 if (grid[pos.x, pos.y].overlay != OverlayType.None) continue;
 
-                if (placed.Any(s =>
-                    Vector2Int.Distance(s, pos) < p.minStarDistance))
+                if (placed.Any(s => Vector2Int.Distance(s, pos) < p.minStarDistance))
                     continue;
 
                 grid[pos.x, pos.y].overlay = OverlayType.Star;
                 placed.Add(pos);
             }
+
+            // Second pass: relax minStarDistance if needed
+            if (placed.Count < p.starCount)
+            {
+                foreach (var pos in candidates)
+                {
+                    if (placed.Count >= p.starCount) break;
+                    if (grid[pos.x, pos.y].overlay != OverlayType.None) continue;
+                    if (placed.Contains(pos)) continue;
+
+                    grid[pos.x, pos.y].overlay = OverlayType.Star;
+                    placed.Add(pos);
+                }
+            }
         }
+
     }
 }
