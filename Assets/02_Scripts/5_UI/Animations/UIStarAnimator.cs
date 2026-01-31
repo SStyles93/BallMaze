f72f1c47d4c2761cf023ec332c83453c2bef16a7
SStyles93 <samuel.styles93@gmail.com>
2026-01-31 11:01:22 -0800

    Addition of Whoosh & glitter sound on stars (EndPannel & Continue Pannel)

--- Assets/02_Scripts/5_UI/Animations/UIStarAnimator.cs
+++ Assets/02_Scripts/5_UI/Animations/UIStarAnimator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
@@ -9,7 +10,12 @@ public class UIStarAnimator : MonoBehaviour
     [SerializeField] private List<Image> starImages = new List<Image>();
     [SerializeField] private ParticleSystem[] starParticles = new ParticleSystem[2];
 
+    [SerializeField] private float delayBetweenStarPop = 0.5f;
+
     private int numberOfStars;
+
+    public event Action OnStarPop;
+    public event Action OnPopFinished;
 
     private void OnEnable()
     {
@@ -54,8 +60,11 @@ public class UIStarAnimator : MonoBehaviour
             star.transform.DOScale(1f, 0.35f).SetEase(Ease.OutBack);
             star.DOFade(1f, 0.3f);
 
-            yield return new WaitForSeconds(0.2f);
+            OnStarPop?.Invoke();
+            yield return new WaitForSeconds(delayBetweenStarPop);
         }
+
+        OnPopFinished?.Invoke();
 
         if (numberOfStars >= 3 && starParticles.Length > 0)
             foreach (var particle in starParticles)
