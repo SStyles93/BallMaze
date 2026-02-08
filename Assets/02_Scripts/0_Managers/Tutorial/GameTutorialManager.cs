03c21626a112a801f4e101aacb923a0676005e33
SStyles93 <samuel.styles93@gmail.com>
2026-02-07 17:50:18 -0800

    Addition of Marbles (Items)

--- Assets/02_Scripts/0_Managers/Tutorial/GameTutorialManager.cs
+++ Assets/02_Scripts/0_Managers/Tutorial/GameTutorialManager.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class GameTutorialManager : MonoBehaviour
 {
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        TutorialManager.Instance.StartTutorial(0);
+        
     }
 }
