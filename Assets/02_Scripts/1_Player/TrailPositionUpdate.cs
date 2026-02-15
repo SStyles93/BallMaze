fbcb7d6f99ef1714c475aa020882c3925dca9cf2
SStyles93 <samuel.styles93@gmail.com>
2026-02-14 17:00:09 -0800

    Udpate materials and trails -> GPU instancing and distort. disabled on Android

--- Assets/02_Scripts/1_Player/TrailPositionUpdate.cs
+++ Assets/02_Scripts/1_Player/TrailPositionUpdate.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TrailPositionUpdate : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float feetOffset = -0.5f;
+    [SerializeField] private GameObject distoritionTrail;
+
+
+    private void Start()
+    {
+#if UNITY_ANDROID
+        distoritionTrail.SetActive(false);
+#endif
+    }
 
     void LateUpdate()
     {
